5d9cab96105224fac0d11d7e897510ec147908cb
Sergey Nemchinov <googoodemon@gmail.com>
2017-07-25 18:36:01 +0700

    rewrite isBetween using generics

--- src/main/java/ru/javawebinar/topjava/util/MealsUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/MealsUtil.java
@@ -18,13 +18,13 @@ import static java.util.function.Function.identity;
  */
 public class MealsUtil {
     public static final List<Meal> MEALS = Arrays.asList(
-            new Meal(0, LocalDateTime.of(2015, Month.MAY, 30, 10, 0), "Завтрак", 500),
+            new Meal(0, LocalDateTime.of(2015, Month.MAY, 30, 8, 0), "Завтрак", 500),
             new Meal(0, LocalDateTime.of(2015, Month.MAY, 30, 13, 0), "Обед", 1000),
             new Meal(0, LocalDateTime.of(2015, Month.MAY, 30, 20, 0), "Ужин", 500),
-            new Meal(0, LocalDateTime.of(2015, Month.MAY, 31, 10, 0), "Завтрак", 1000),
+            new Meal(0, LocalDateTime.of(2015, Month.MAY, 31, 9, 0), "Завтрак", 1000),
             new Meal(0, LocalDateTime.of(2015, Month.MAY, 31, 13, 0), "Обед", 500),
             new Meal(0, LocalDateTime.of(2015, Month.MAY, 31, 20, 0), "Ужин", 510),
-            new Meal(1, LocalDateTime.of(2015, Month.MAY, 31, 10, 0), "Завтрак", 801),
+            new Meal(1, LocalDateTime.of(2015, Month.MAY, 31, 7, 0), "Завтрак", 801),
             new Meal(1, LocalDateTime.of(2015, Month.MAY, 31, 13, 0), "Обед", 401),
             new Meal(1, LocalDateTime.of(2015, Month.MAY, 31, 20, 0), "Ужин", 411),
             new Meal(1, LocalDateTime.of(2015, Month.MAY, 30, 10, 0), "Завтрак", 501),
@@ -35,10 +35,10 @@ public class MealsUtil {
     public static final int DEFAULT_CALORIES_PER_DAY = 2000;
 
     public static void main(String[] args) {
-        List<MealWithExceed> mealsWithExceeded = getFilteredWithExceeded(MEALS, LocalTime.of(7, 0), LocalTime.of(12, 0), 2000);
+        List<MealWithExceed> mealsWithExceeded = getFilteredWithExceeded(MEALS, LocalTime.of(10, 0), LocalTime.of(12, 0), 2000);
         mealsWithExceeded.forEach(System.out::println);
 
-        System.out.println(getFilteredWithExceededByCycle(MEALS, LocalTime.of(7, 0), LocalTime.of(12, 0), DEFAULT_CALORIES_PER_DAY));
+        System.out.println(getFilteredWithExceededByCycle(MEALS, LocalTime.of(10, 0), LocalTime.of(12, 0), DEFAULT_CALORIES_PER_DAY));
     }
 
     public static List<MealWithExceed> getWithExceeded(Collection<Meal> meals, int caloriesPerDay) {
